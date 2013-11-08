780099fb4ab23288dcc1e808b2f838619982337e
Lars Greiss <kufikugel@googlemail.com>
2013-11-08 20:28:53 +0100

    fb: volume rocker control (wake up)
    Change-Id: I60f5f88e32a129afda80f5b0f4f41cb0a322b691
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2646,6 +2646,12 @@ public final class Settings {
         public static final String SCREENSHOT_SOUND = "screenshot_sound";
 
         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_WAKE_SCREEN = "volume_wake_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
