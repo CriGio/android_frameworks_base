9ad71ce2c4495dac7d1f7b742bb945738acb790f
Crostantin <crostantin@gmail.com>
2014-11-13 03:41:44 +0100

    Disable/Enable screenshot sound [1/2]
    ref-https://github.com/DirtyUnicorns/android_frameworks_base/commit/8c5fec2b6d30f5014b25954ee4dd4e061cf7e243

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2646,6 +2646,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
